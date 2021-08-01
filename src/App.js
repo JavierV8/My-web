e888a90c9356a6c60f6a1fe33b540aa8f7daa189
Javier Sanchez <javier.san.mail@gmail.com>
2021-08-01 14:18:51 +0200

    moved pages components from components into pages directory

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import Home from './components/Home';
-import About from './components/About';
-import Projects from './components/Projects';
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
 
 function App() {
   return (
