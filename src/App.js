f738091ac521b6b5d8d7cddccfc15f6ddbfb1d8c
Javier Sanchez <javier.san.mail@gmail.com>
2021-08-01 14:17:29 +0200

    created projects page component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Home from './components/Home';
 import About from './components/About';
+import Projects from './components/Projects';
 
 function App() {
   return (
     <div className="App">
       <Home />
       <About />
+      <Projects />
     </div>
   );
 }
---
+++ src/components/Projects.jsx
@@ -0,0 +1,11 @@
+import '../styles/projects.css';
+
+function Projects() {
+  return (
+    <div className="Home">
+      <h1>PROJECTS</h1>
+    </div>
+  );
+}
+
+export default Projects;
